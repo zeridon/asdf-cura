c52a508733b0e535380d9f485b6256f97fcbb18c
Vladimir Vitkov <vvitkov@gmail.com>
2024-10-28 13:16:00 +0200

    remove debug

--- lib/utils.bash
+++ lib/utils.bash
@@ -56,7 +56,6 @@ install_version() {
 	local version="$2"
 	local install_path="${3%/bin}/bin"
 
-	set -x
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
 	fi
